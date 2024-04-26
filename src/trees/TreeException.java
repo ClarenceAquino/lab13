d5476ebf853d2f97ce3d06ba06c4481aef6fc726
ClarenceAquino <aquinoclarence9@gmail.com>
2024-04-26 16:54:19 -0700

    new updated TreeNode Class

--- src/trees/TreeException.java
+++ src/trees/TreeException.java
@@ -1,9 +1,13 @@
 package trees;
 
-public class TreeException extends ???
+import java.io.IOException;
+
+public class TreeException extends RuntimeException
 {
 	public TreeException(String s)
 	{
+		super(s);
+		
 		// Pass s into a superclass ctor.
 	}
 }
